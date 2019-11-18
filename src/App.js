a2bed2d318b20b4282ede3e60c284482c72873d6
tushar ghodadra <tusharghodadra@gmail.com>
2019-11-19 00:22:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ReactSearchBox from 'react-search-box'
+
+export default class App extends Component {
+    
+    state = {
+      data:[],
+    };
+  
+       contactsData= [];
+	   componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+  
+
+  render() {
+	  
+	
+	  
+	  var search=[];
+	  let valuesTitle= this.state.data.map(el=> search.push({key:el.title,value:el.body}));
+	 
+
+    return (
+	<div>
+      <ReactSearchBox
+        placeholder="Placeholder"
+        value="search"
+        data={search}
+        callback={record => console.log(record)}
+      />	
+         <center > < h1 > Contact List </h1></center > {
+            this.state.data.map(el=> ( 
+			
+			<div class = "card" >
+                <div class = "card-body" >                
+				<h6 class = "card-subtitle mb-2 text-muted" > { el.title } </h6>
+				<p class = "card-text" > { el.body } </p> 
+				</div>
+				</div>
+            ))	  
+			} 
+	  </div>
+	  
+    )
+	
+  }
 }
-
-export default App;
